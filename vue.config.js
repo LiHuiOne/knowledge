b227adc9a48b9811d5bff31dbd5405fa3c914ba9
lihui_lh <OnfEnSQSwtepmbOgttHC>
2021-09-08 10:20:32 +0800

    feat(安装热更新插件webpack-dev-server)代码提交

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,13 @@
 module.exports = {
   lintOnSave: false,//你是否使用eslint规范
   productionSourceMap: false,//打包时去掉map文件
-  publicPath:'./',//解决打包时css和js以及静态文件路径不正确问题
+  publicPath:'/',//解决打包时css和js以及静态文件路径不正确问题（vue-cli3的是./）
   devServer: {
     port: 3033,
     host: "0.0.0.0",
     https: false, // https:{type:Boolean}
     open: true, // 配置自动启动浏览器
+    disableHostCheck: true, //webpack4.0 开启热更新
     //配置多个代理的话，直接在proxy写就可以
     //productionSourceMap: false,//打包时去掉map文件
     // proxy: {
@@ -20,5 +21,8 @@ module.exports = {
     //     }
     //   }
     // },
+  },
+  css:{
+
   }
 };
