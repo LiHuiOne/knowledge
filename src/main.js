f07675a0a07e8509994787601275885593d25c16
lihui_lh <OnfEnSQSwtepmbOgttHC>
2021-09-17 16:48:32 +0800

    将message挂载到原型上

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import router from './router'
 import store from './store'
 import api from '@/service/interface.js'
 import ElementPlus from 'element-plus'
+import { ElMessage } from 'element-plus'
 import 'element-plus/dist/index.css'
 const app = createApp(App)
 //将api方法挂载到原型上
 app.config.globalProperties.$api=api
+app.config.globalProperties.$message=ElMessage
 app.use(ElementPlus)
 .use(api)
 .use(store)
